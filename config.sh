3dc5acfbc6c9e6274fb0a87aeb27f7f5eccf7e9a
Steven Chen <cac3213@gmail.com>
2017-07-05 22:21:46 -0500

    Change the toolchain to arm64. Change the api to 21.

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 system=$(uname -s | tr 'DL' 'dl')-$(uname -m)
-gcc_version=4.6
-toolchain=arm-linux-androideabi-$gcc_version
-platform=android-14
-PYTHONPATH=/opt/ros/indigo/lib/python2.7/dist-packages:$PYTHONPATH
+gcc_version=4.9
+toolchain=aarch64-linux-android-$gcc_version
+platform=android-21
+PYTHONPATH=/opt/ros/kinetic/lib/python2.7/dist-packages:$PYTHONPATH
 # Enable this value for debug build
 CMAKE_BUILD_TYPE=Debug
 # Enable this if you need to use pluginlib in Android.
 # The plugins will be statically linked
-use_pluginlib=1
+use_pluginlib=0
 
